6434491a082f33e2f2d8c1704db9f9320d2fbff6
restradaodoogrp <88213147+restradaodoogrp@users.noreply.github.com>
2022-03-25 12:43:35 -0600

    Update init_informix.sh

--- init_informix.sh
+++ init_informix.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-FILE=//opt/IBM/Informix_Software_Bundle/ol_informix1410.ksh
-if test -f "$FILE"; then
-    echo $dbnameinformix"    onsoctcp    "$ipinformix"    "$puertoinformix > /opt/IBM/Informix_Software_Bundle/ol_informix1410.ksh
-else
-    echo linea6
-fi
+FILE=/opt/IBM/Informix_Software_Bundle/etc/sqlhosts.ol_informix1410
 FILE2=/opt/IBM/Informix_Software_Bundle/etc/onconfig.ol_informix1410
 DBSERVERNAME="DBSERVERNAME ol_informix1410"
 DBSERVERNAMEALIAS="DBSERVERALIASES dr_informix1410, lo_informix1410"
 REPLACEDBSERVERNAME="DBSERVERNAME 999999"
 REPLACEDBSERVERNAMEALIAS="DBSERVERALIASES False"
+
+
+if [[ "${dbnameinformix}" ]]; then
+
+if [[ "${ipinformix}" ]]; then
+
+if [[ "${puertoinformix}" ]]; then
+
+if test -f "$FILE"; then
+    echo $dbnameinformix"    onsoctcp    "$ipinformix"    "$puertoinformix > $FILE
+else
+    echo no encontro el 
+fi
 
 if test -f "$FILE2"; 
  then
@@ -17,11 +25,15 @@ if test -f "$FILE2";
     sed -i "s/$DBSERVERNAMEALIAS/$REPLACEDBSERVERNAMEALIAS/" $FILE2
     sed -i "s/999999/${dbnameinformix}/" $FILE2
 else
-    echo linea18
+    echo no encontro el archivo
 fi
 
-if [[ "${dbnameinformix}" ]]; then
-    echo ${dbnameinformix}
 else 
-    echo linea25
+    echo falta nombre db informix
+fi
+else 
+    echo falta ip informix
+fi
+else 
+    echo falta puerto informix
 fi
